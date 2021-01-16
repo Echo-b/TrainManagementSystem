8db6b808a0a0b2327102806625c34e24949fda47
Echo <62658068+Echo-b@users.noreply.github.com>
2021-01-16 18:54:29 +0800

    改变乘客信息函数的返回值用于测试

--- TrainManagementSystem/search_realated_information.cpp
+++ TrainManagementSystem/search_realated_information.cpp
@@ -1,8 +1,7 @@
 ﻿#include "HeadFile.h"
-Ppassenger* search_realated_Pasinf(PassengerTableType& passenger_table,char * input_realated_info) {
+Status search_realated_Pasinf(PassengerTableType& passenger_table,char * input_realated_info,Ppassenger &passenger_point[10]) {
 	//参数为乘客数据表和一个输入的参数，可以是紧急联系人或者证件号，姓名等，查找成功返回一个结构体指针数组首地址
 	int is_final_number=0;//用于统计匹配人数 
-	Ppassenger passenger_point[10] = {NULL};//声明一个结构体指针数组，用于储存查找结果
 	for (int i = 0; i < passenger_table.size; i++) {
 		if (strcmp(passenger_table.passengers->emergency_contact, input_realated_info) == 0 || strcmp(passenger_table.passengers->id_name, input_realated_info) == 0
 			|| strcmp(passenger_table.passengers->id_number, input_realated_info) == 0 || strcmp(passenger_table.passengers->id_number, input_realated_info) == 0) {
@@ -11,7 +10,7 @@ Ppassenger* search_realated_Pasinf(PassengerTableType& passenger_table,char * in
 	}
 	if (is_final_number == 0) {
 		outtextxy(0, 0, _T("未找到匹配数据"));//输出位置后期确定
-		return NULL;
+		return ERROR;
 	}
-	return passenger_point;
+	return OK;
 }
