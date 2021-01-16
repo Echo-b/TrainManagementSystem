00c00ada94fcfea71bb85884cc9c376db70f9b8d
Echo <62658068+Echo-b@users.noreply.github.com>
2021-01-16 17:08:03 +0800

    Add files via upload
    乘客信息查找函数

---
+++ TrainManagementSystem/search_realated_information.cpp
@@ -0,0 +1,17 @@
+﻿#include "HeadFile.h"
+Ppassenger* search_realated_Pasinf(PassengerTableType& passenger_table,char * input_realated_info) {
+	//参数为乘客数据表和一个输入的参数，可以是紧急联系人或者证件号，姓名等，查找成功返回一个结构体指针数组首地址
+	int is_final_number=0;//用于统计匹配人数 
+	Ppassenger passenger_point[10] = {NULL};//声明一个结构体指针数组，用于储存查找结果
+	for (int i = 0; i < passenger_table.size; i++) {
+		if (strcmp(passenger_table.passengers->emergency_contact, input_realated_info) == 0 || strcmp(passenger_table.passengers->id_name, input_realated_info) == 0
+			|| strcmp(passenger_table.passengers->id_number, input_realated_info) == 0 || strcmp(passenger_table.passengers->id_number, input_realated_info) == 0) {
+			passenger_point[is_final_number++] = passenger_table.passengers;
+		}
+	}
+	if (is_final_number == 0) {
+		outtextxy(0, 0, _T("未找到匹配数据"));//输出位置后期确定
+		return NULL;
+	}
+	return passenger_point;
+}
