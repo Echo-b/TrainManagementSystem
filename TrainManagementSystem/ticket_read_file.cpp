8c98ee44739c7c7086c81ea3ad2517505b68b900
Echo <62658068+Echo-b@users.noreply.github.com>
2021-01-18 15:28:05 +0800

    车票预定文件的读取函数

---
+++ TrainManagementSystem/ticket_read_file.cpp
@@ -0,0 +1,27 @@
+#include"HeadFile.h"
+//此函数用于从文件读取乘客信息，第一个函数为读取后所放的位置，第二个为文件名。返回值当返回OK时成功，当返回值为ERROR时读取失败
+Status ticket_read_file(TicketTableType& ticket_table, const char* file_name)
+{
+	FILE* fp;
+	int i = 0;//第i个乘客
+	if (!(fp = fopen(file_name, "r")))return ERROR;
+	while (fscanf(fp, "%s", ticket_table.trains_ticket[i].order_number) != -1) {
+		fscanf(fp,"%4d%2d%2d", &ticket_table.trains_ticket[i].travel_data.year, &ticket_table.trains_ticket[i].travel_data.month, &ticket_table.trains_ticket[i].travel_data.day);
+		fscanf(fp, "%s", ticket_table.trains_ticket[i].train_name);
+		fscanf(fp, "%s", ticket_table.trains_ticket[i].start_station);
+		fscanf(fp, "%s", ticket_table.trains_ticket[i].end_station);
+		fscanf(fp, "%s", ticket_table.trains_ticket[i].id_type);
+		fscanf(fp, "%s", ticket_table.trains_ticket[i].id_number);
+		fscanf(fp, "%s", ticket_table.trains_ticket[i].id_name);
+		fscanf(fp, "%d:%d", &ticket_table.trains_ticket[i].start_time.hour, &ticket_table.trains_ticket[i].start_time.minute);
+		fscanf(fp, "%d:%d", &ticket_table.trains_ticket[i].arrival_time.hour, &ticket_table.trains_ticket[i].arrival_time.minute);
+		fscanf(fp, "%s", ticket_table.trains_ticket[i].seat_level);
+		fscanf(fp, "%d", &ticket_table.trains_ticket[i].seat_number);
+		fscanf(fp, "%f", &ticket_table.trains_ticket[i].cost);
+		i++;
+		ticket_table.num_of_book++;
+		//todu加入realloc函数，防止输入时超过最大容量
+	}
+	fclose(fp);
+	return OK;
+}
